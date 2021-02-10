c1aa9aad6ae5a6690d29e11fab402efdcf898e30
rktel <rakinakuy@hotmail.com>
2021-02-10 01:58:01 -0500

    third commit

--- LORA_BASIC_NODE.ino
+++ LORA_BASIC_NODE.ino
@@ -1,4 +1,4 @@
-
+  
 #include <SPI.h>
 #include <RH_RF95.h>
 
@@ -27,7 +27,7 @@ void loop()
 {
   if (runEvery(5000)) {
     Serial.print("Sending: ");
-    String payload = deviceID + String(random(20, 40));
+    String payload = deviceID + String(random(20, 40)) + "\n";
     Serial.println(payload);
     rf95.send(payload.c_str(), payload.length());
     rf95.waitPacketSent();

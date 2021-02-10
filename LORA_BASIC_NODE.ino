ab2a0417f07c113354c587c702129e4ef5784bfc
rktel <rakinakuy@hotmail.com>
2021-02-09 20:05:48 -0500

    first commit

---
+++ LORA_BASIC_NODE.ino
@@ -0,0 +1,45 @@
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// Singleton instance of the radio driver
+RH_RF95 rf95;
+float frequency = 868.0;
+int txPower = 20;
+
+long prevMillis = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  //while (!Serial) ; // Wait for serial port to be available
+  Serial.println("Start LoRa Client");
+  if (!rf95.init())
+    Serial.println("init failed");
+  // Setup ISM frequency
+  loraSetup();
+
+}
+
+void loop()
+{
+  if (runEvery(5000)) {
+    Serial.print("Sending: ");
+    String payload = "LORD0101" + String(random(20, 40));
+    Serial.println(payload);
+    rf95.send(payload.c_str(), payload.length());
+    // rf95.waitPacketSent();
+  }
+
+}
+void loraSetup() {
+  rf95.setTxPower(txPower, false);
+  rf95.setFrequency(frequency);
+}
+bool runEvery(long _interval) {
+  if (millis() - prevMillis > _interval) {
+    prevMillis = millis();
+    return true;
+  }
+  return false;
+}
